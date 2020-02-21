89c61f2c06626c46baa0eda68c52fadf17dd614e
Taras Plaskon <tarasplaskon@gmail.com>
2020-02-21 13:22:51 +0200

    second commit

---
+++ keys/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB_URI : 'mongodb+srv://taras:Mw1l6eMxzdO6J6ve@cluster0-vg0ap.mongodb.net/shop', // public link which access us to connect to DB
+    SESSION_SECRET: 'some secret value'
+}
